80ef2bc2d49600e80dcb72e9ac0ade2a0a868d47
Bruno <brunoowap@gmail.com>
2022-07-18 16:41:02 -0300

    adicionando scss nas informações das receita

--- src/components/molecules/FoodInfo/index.jsx
+++ src/components/molecules/FoodInfo/index.jsx
@@ -1,8 +1,29 @@
 import "./info.scss";
+import Timer from "/assets/Timer.svg";
+import ForkKnife from "/assets/ForkKnife.svg";
 function FoodInfo() {
   return (
     <div className="foodInfo">
       <h2 className="foodInfo-title">Health Japanese Fried Rice</h2>
+      <div className="foodSpecifications">
+        <div className="foodSpecifications-container">
+          <div className="foodSpecifications-content ">
+            <img src={Timer} />
+            <span className="foodSpecifications-container--titleTimer">
+              COOK TIME
+            </span>
+          </div>
+          <span className="foodSpecifications-container--titleMinutes">
+            15 MINUTES
+          </span>
+        </div>
+        <div className="foodSpecifications-content">
+          <img src={ForkKnife} />
+          <span className="foodSpecifications-container--titleType">
+            Chicken
+          </span>
+        </div>
+      </div>
     </div>
   );
 }
